f7136ff7bbe42d3e786e6f07415debec468fb5bc
NHThang <thangnh2812@gmail.com>
2026-07-01 10:26:27 +0700

    feat: Integrate DB connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import {connectDB} from './config/db.js';
 
-// Load environment variables
+
 dotenv.config();
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,11 +17,10 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use("/", (req,res)=>{
+  res.json("Backend is running");
+})
 
-
-app.get('/get', (req, res) => {
-  res.send('Server is running');
-});
 
 app.listen(PORT, ()=>{
   console.log(`Server is running at the port ${PORT}`);
